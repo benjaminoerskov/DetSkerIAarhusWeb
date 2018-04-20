5235147965ac90ea59281597635ca6ec72c7277a
benjaminoerskov <ben.oe@icloud.com>
2018-04-20 15:16:26 +0200

    mere crap

--- src/About.tsx
+++ src/About.tsx
@@ -8,7 +8,7 @@ class About extends React.Component {
         <p className="App-intro">
           Her kan man bare lige læse at det er mig der har lavet det...
         </p>
-        <Link to="/feed">Tryk her for at se det dejlige</Link>
+        <Link to="/feed">Tryk her for at komme hen til det dejlige</Link>
       </div>
     );
   }
