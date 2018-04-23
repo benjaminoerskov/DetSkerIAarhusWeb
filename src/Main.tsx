9f2bc5946530668012bbe53f26b1d810d45c0e1f
benjaminoerskov <ben.oe@icloud.com>
2018-04-23 13:15:45 +0200

    refactor

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,15 +2,15 @@ import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import AboutPage from './components/About/aboutPage';
-import { IRouterLinkElementProps } from './components/Common/navigationBar';
-import HomePage from './components/Home/homePage';
+import { IRouterLinkElementProps } from './components/Common/rootComponent';
+// import HomePage from './components/Home/homePage';
 import DetailPage from './components/OccurrenceFeed/occurrenceDetail';
 import FeedPage from './components/OccurrenceFeed/occurrenceFeed';
 
 const Main = () => (
     <main>
       <Switch>
-        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/' component={FeedPage}/>
         <Route path='/feed' component={FeedPage}/>
         <Route path='/about' component={AboutPage}/>
         <Route path='/occurrences/:id' component={DetailPage}/>
--- src/components/Common/rootComponent.tsx
+++ src/components/Common/rootComponent.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter, NavLink} from 'react-router-dom';
 
-import Main from '../../Main';
-import NavigationBar from './navigationBar';
+import Main, { routes } from '../../Main';
 
 class RootComponent extends React.Component {
     
@@ -18,39 +17,39 @@ class RootComponent extends React.Component {
     }
 }
 
-// const NavigationBar = () => {
-//     return (
-//         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//             <NavLink className="navbar-brand" to={"/"} >Home</NavLink>
-//             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//                 <span className="navbar-toggler-icon"></span>
-//             </button>
-//             <div className="collapse navbar-collapse" id="navbarNav">
-//                 <ul className="navbar-nav">
-//                     <ConstructNavLinkElements routes={routes} />
-//                 </ul>
-//             </div>
-//         </nav>
-//     )
-// }
+const NavigationBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <NavLink className="navbar-brand" to={"/"} >Home</NavLink>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <ConstructNavLinkElements routes={routes} />
+                </ul>
+            </div>
+        </nav>
+    )
+}
 
-// export interface IRouterLinkElementProps {
-//     routeLink: string;
-//     name: string;
-// }
+export interface IRouterLinkElementProps {
+    routeLink: string;
+    name: string;
+}
 
-// const NavLinkElement = (props:IRouterLinkElementProps) => (
-//     <li className="nav-item">
-//     <NavLink className="nav-link" activeClassName="active" to={props.routeLink} >{props.name}</NavLink>
-//   </li>
-// )
+const NavLinkElement = (props:IRouterLinkElementProps) => (
+    <li className="nav-item">
+    <NavLink className="nav-link" activeClassName="active" to={props.routeLink} >{props.name}</NavLink>
+  </li>
+)
 
-// const ConstructNavLinkElements = (props: { routes: IRouterLinkElementProps[] }): JSX.Element => (
-//     <>
-//         {props.routes.map((data) => {
-//             return <NavLinkElement routeLink={data.routeLink} name={data.name} key={data.routeLink} />
-//         })}
-//     </>
-// )
+const ConstructNavLinkElements = (props: { routes: IRouterLinkElementProps[] }): JSX.Element => (
+    <>
+        {props.routes.map((data) => {
+            return <NavLinkElement routeLink={data.routeLink} name={data.name} key={data.routeLink} />
+        })}
+    </>
+)
 
 export default RootComponent;
